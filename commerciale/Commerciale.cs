2970c577f3de1ccd668d84cc0a976380b6b66fa0
Manon0206 <manonf1.pro@gmail.com>
2024-03-21 08:42:35 +0100

    Ajoutez des fichiers projet.

---
+++ commerciale/Commerciale.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+class Commercial
+{
+    private string nom;
+    private string prenom;
+    private int anneeNaissance;
+    private Categorie categorie;
+
+    public Commercial(string nom, string prenom, int anneeNaissance, Categorie categorie)
+    {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.anneeNaissance = anneeNaissance;
+        this.categorie = categorie;
+    }
+
+    public string Nom
+    {
+        get { return nom; }
+        set { nom = value; }
+    }
+
+    public string Prenom
+    {
+        get { return prenom; }
+        set { prenom = value; }
+    }
+
+    public int AnneeNaissance
+    {
+        get { return anneeNaissance; }
+        set { anneeNaissance = value; }
+    }
+
+    public int CalculAge()
+    {
+        return DateTime.Now.Year - anneeNaissance;
+    }
+
+    public override string ToString()
+    {
+        return $"Nom: {nom}, Prénom: {prenom}, Âge: {CalculAge()}, Catégorie: {categorie}";
+    }
+
+    public void Compare(Commercial c)
+    {
+        if (this.nom.Split(' ')[0].Equals(c.nom.Split(' ')[0]))
+            Console.WriteLine($"{this.nom} et {c.nom} ont le même nom de famille.");
+        else
+            Console.WriteLine($"{this.nom} et {c.nom} n'ont pas le même nom de famille.");
+    }
+}
+
+class Categorie
+{
+    private string codeCategorie;
+    private string libCategorie;
+
+    public Categorie(string codeCategorie, string libCategorie)
+    {
+        this.codeCategorie = codeCategorie;
+        this.libCategorie = libCategorie;
+    }
+
+    public string CodeCategorie
+    {
+        get { return codeCategorie; }
+        set { codeCategorie = value; }
+    }
+
+    public string LibCategorie
+    {
+        get { return libCategorie; }
+        set { libCategorie = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"Code: {codeCategorie}, Libellé: {libCategorie}";
+    }
+}
+
+class Service
+{
+    private string nomService;
+    private Commercial[] commerciaux;
+    private int effectif;
+
+    public Service(string nomService)
+    {
+        this.nomService = nomService;
+        commerciaux = new Commercial[10];
+        effectif = 0;
+    }
+
+    public void AjouterCom(Commercial c)
+    {
+        if (effectif < 10)
+        {
+            commerciaux[effectif] = c;
+            effectif++;
+        }
+        else
+        {
+            Console.WriteLine("Impossible d'ajouter plus de commerciaux.");
+        }
+    }
+
+    public void AfficherCommerciaux()
+    {
+        Console.WriteLine($"Commerciaux du service {nomService}:");
+        for (int i = 0; i < effectif; i++)
+        {
+            Console.WriteLine(commerciaux[i]);
+        }
+    }
+}
---
+++ commerciale/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Categorie salarie = new Categorie("001", "Salarie");
+        Categorie independant = new Categorie("002", "Indépendant");
+
+        Commercial com1 = new Commercial("Chabault", "Mathis", 1975, salarie);
+        Commercial com2 = new Commercial("Boulec", "Tom", 1985, independant);
+        Commercial com3 = new Commercial("Habert", "Manon", 1977, salarie);
+
+        Console.WriteLine("Informations des commerciaux :");
+        Console.WriteLine(com1);
+        Console.WriteLine(com2);
+        Console.WriteLine(com3);
+
+        com1.Compare(com2);
+
+        Service national = new Service("National");
+        Service international = new Service("International");
+
+        national.AjouterCom(com1);
+        national.AjouterCom(com2);
+        national.AjouterCom(com3);
+
+        international.AjouterCom(com2);
+        international.AjouterCom(com3);
+
+        Console.WriteLine("\nCommerciaux du service National :");
+        national.AfficherCommerciaux();
+
+        Console.WriteLine("\nCommerciaux du service International :");
+        international.AfficherCommerciaux();
+    }
+}
